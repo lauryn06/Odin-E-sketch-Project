abc1b31f62213bd7007fe9dcbb84fef4a8063620
lauryn <laurynmwase06@gmail.com>
2025-06-07 11:11:54 +0200

    pushing css,html and js files to github

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+const btn=document.querySelector("button");
+const container=document.querySelector(".container");
+
+clicking=()=>{
+  const v= window.prompt("How many grid squares per side: ");
+const num = Number(v);
+
+if(num <= 100 && num > 0){
+    container.innerHtml="";
+    const containerWidth = container.clientWidth;
+    const squareSize = containerWidth / num;
+    
+
+for(let i=0; i<num*num; i++){
+
+   const div=document.createElement('div');
+   div.classList.add('square');
+
+       div.style.width = `${squareSize}px`;
+      div.style.height = `${squareSize}px`;
+
+   container.appendChild(div);
+
+    };
+}
+
+else{
+    alert("Please enter a number between 1 and 100.");
+}
+};
+
+
+
